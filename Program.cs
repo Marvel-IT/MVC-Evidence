0b289033e8ecc0cdbf5221782906cec297ee02d4
Tomas Kuzelka <tkuzelka@gmail.com>
2023-05-16 20:56:55 +0200

    style css

--- Program.cs
+++ Program.cs
@@ -8,7 +8,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<MvcOsobaContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("MvcOsobaContext") ?? throw new InvalidOperationException("Connection string 'MvcOsobaContext' not found.")));
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
@@ -20,11 +19,11 @@ using (var scope = app.Services.CreateScope())
     SeedData.Initialize(services);
 }
 
-// Configure the HTTP request pipeline.
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    
     app.UseHsts();
 }
 
